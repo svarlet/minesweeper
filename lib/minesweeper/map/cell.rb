336624b96eb3d3cabb0344afdf5f972d4d6b01e6
Sébastien Varlet <sebastien.varlet@plumbee.co.uk>
2015-02-12 18:20:09 +0000

    Fixes a merge conflict.

--- lib/minesweeper/map/cell.rb
+++ lib/minesweeper/map/cell.rb
@@ -1,52 +1,46 @@
 require 'minesweeper/map/hidden_state'
 
 module Minesweeper
-  module Map
-    class Cell < CellState
-      # STATES
-      # ---------------------------------------------------------
-      # CURRENT	|	ACTION 	|	STATES			|	ACTION
-      # ---------------------------------------------------------
-      # hidden	|	reveal	|	revealed 		|
-      # hidden	|	reveal	|	revealed 		|	end game
-      # hidden	|	flag 	|	flagged			|
-      # flagged	|	reveal	|	revealed		|
-      # flagged	|	reveal	|	revealed		|
-      # flagged	|	unflag	|	hidden			|
-      # ---------------------------------------------------------
+	module Map
+		class Cell < CellState
+			# STATES
+			# ---------------------------------------------------------
+			# CURRENT	|	ACTION 	|	STATES			|	ACTION
+			# ---------------------------------------------------------
+			# hidden	|	reveal	|	revealed 		|	trigger
+			# hidden	|	flag 	|	flagged			|
+			# flagged	|	reveal	|	revealed		|	trigger
+			# flagged	|	unflag	|	hidden			|
+			# ---------------------------------------------------------
 
-      attr_accessor :current_state
-      attr_reader :mines_around, :hidden_state, :flagged_state, :revealed_state
+			attr_accessor :current_state
+			attr_reader :mines_around, :hidden_state, :flagged_state, :revealed_state
 
-      def initialize(has_mine = false, mines_around = 0)
-        @hidden_state = HiddenState.new(self)
-        @flagged_state = FlaggedState.new(self)
-        @revealed_state = RevealedState.new(self)
+			def initialize(mine, mines_around = 0)
+				@hidden_state = HiddenState.new(self)
+				@flagged_state = FlaggedState.new(self)
+				@revealed_state = RevealedState.new(self)
 
-        @current_state = @hidden_state
-        @has_mine = has_mine
-        @mines_around = mines_around
-      end
+				@current_state = @hidden_state
+				@mine = mine
+				@mines_around = mines_around
+			end
 
-      def flag
-        current_state.flag
-      end
+			def flag
+				current_state.flag
+			end
 
-      def reveal
-        current_state.reveal
-      end
+			def reveal
+				current_state.reveal
+			end
 
-      def unflag
-        current_state.unflag
-      end
+			def unflag
+				current_state.unflag
+			end
 
-      def has_mine?
-        @has_mine
-      end
-
-      def to_s
-        @current_state.to_s
-      end
-    end
-  end
+			def to_s
+        		@current_state.to_s
+      		end
+		end
+	end
 end
