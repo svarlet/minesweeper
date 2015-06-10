76d335eaf4d318e62ca1ad16afa436ee4d712a6d
Sebastien Varlet <sebastien.varlet@plumbee.co.uk>
2015-06-10 23:35:08 +0100

    Requires the mine_coordinate_factory file in the main minesweeper file

--- lib/minesweeper.rb
+++ lib/minesweeper.rb
@@ -1,5 +1,6 @@
 require 'minesweeper/version'
 require 'minesweeper/minefield'
 require 'minesweeper/explosives/explosion_error'
+require 'minesweeper/explosives/mine_coordinates_factory'
 require 'minesweeper/explosives/mine_layer'
 
